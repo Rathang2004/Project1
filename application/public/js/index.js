49299f7d88b8b794cdbf4ab5c0bcf755a69d6106
“Rathang2004git config --global user.email “rikimpandit@gmail.com <rikimpandit@gmail.com>
2023-04-13 12:01:01 -0700

    Final Version of all files js of registration, index, css of registration, index html of index, registration

---
+++ application/public/js/index.js
@@ -0,0 +1,57 @@
+var url = 'https://jsonplaceholder.typicode.com/albums/2/photos/';
+
+    document.getElementById("display").innerHTML = 'Number of Photos: <br><br> 50';
+    function fadeOut(ev)
+{
+
+    let timer = setInterval(function ()
+    {
+        document.getElementById("display").innerHTML = "Number of Photos:";
+        document.getElementById("display1").innerHTML = (50 - timer);
+        clearInterval(timer);
+    }, 200)
+    ev.currentTarget.remove();
+
+}
+function buildCard(data)
+{
+    var cardDiv = document.createElement("div");
+    cardDiv.setAttribute("class","product-card");
+
+    var imgTag = document.createElement("img");
+    imgTag.setAttribute("class", "product-img");
+    imgTag.setAttribute("src", data.url);
+
+    var titleTag = document.createElement("p");
+    titleTag.setAttribute("class", "product-title");
+    titleTag.appendChild(document.createTextNode(data.title));
+
+    var productDiv = document.createElement("div");
+    productDiv.setAttribute("class", "product-info");
+
+    productDiv.appendChild(titleTag);
+    cardDiv.appendChild(imgTag)
+    cardDiv.appendChild(productDiv);
+    cardDiv.addEventListener("click", fadeOut);
+    return cardDiv;
+
+}
+async function fetchWithDOMAPI()
+{
+    try {
+        var response = await fetch(url);
+        var data = await response.json();
+        var elements = data.map(buildCard);
+        document.getElementById("product-list").append(...elements);
+    }   catch (error){
+        console.log(error);
+    }
+}
+fetchWithDOMAPI();
+
+
+
+
+
+
+
---
+++ application/public/js/registration.js
@@ -0,0 +1,138 @@
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmpassword');
+const form = document.getElementById("form");
+const errorElement = document.getElementsByClassName('error');
+
+const errorClass = (element,message) =>
+{
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+const okClass = (element) =>
+{
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
+
+form.addEventListener("submit", (e) =>
+{
+    if(!validateForm())
+    {
+        e.preventDefault();
+    }
+})
+/*yo check the onsumbit functanitly*/
+
+function validateForm()
+{
+    validateUsername();
+    validatePassword();
+    validateEmail();
+    validateConfirmPassword();
+    if (document.querySelectorAll('.success').length === 4) {
+        // submit the form
+        form.submit();
+    }
+}
+function validateUsername()
+{
+    if((username.value === "") || (username.value === null))
+    {
+        errorClass(username, 'Username cannot be empty');
+    }
+    else if(username.value.length < 3)
+    {
+        errorClass(username, 'Username must be atleast 3 characters');
+    }
+    else if(!/^[a-zA-Z]/.test(username.value))
+    {
+        errorClass(username, 'Username must begin with [a-zA-Z]');
+    }
+    else
+    {
+        okClass(username);
+    }
+}
+
+function validateEmail()
+{
+    if(email.value === "")
+    {
+        errorClass(email, 'Email can not be empty');
+    }
+/**    else if(email.value <  3)
+    {
+        errorClass(email, 'Email entered is invalid');
+    }*/
+    else
+    {
+        okClass(email);
+    }
+}
+
+function validatePassword()
+{
+    const specialCharacters = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    if (password.value === "") {
+        errorClass(password, 'Password can not be empty');
+    } else if (password.value.length < 8) {
+        errorClass(password, 'Password has to be atleast 8 characters');
+    }
+    else if(!/[a-z]/.test(password.value) || !/[A-Z]/.test(password.value) || !/\d/.test(password.value) || !specialCharacters.test(password.value))
+    {
+        errorClass(password,'Password must contain 1 uppercase letter, 1 lowercase, 1 number, and one of the following characters: / * - + ! @ # $ ^ & ~ [ ]');
+    }
+    else
+    {
+        okClass(password);
+    }
+}
+
+function validateConfirmPassword()
+{
+    if(confirmPassword.value === '')
+    {
+        errorClass(confirmPassword, 'Confirm password section can not be empty');
+    }
+    else if(confirmPassword.value != password.value)
+    {
+        errorClass(confirmPassword, 'Password entered doesn\'t match with the password above');
+    }
+    else
+    {
+        okClass(confirmPassword);
+    }
+}
+
+
+
+
+
+/*
+
+* first of all, figure out the checkbox stuff
+* do the css
+* figure out how to send a email push
+* figure
+
+ */
+
+
+
+
+
+
+
+
+
+
+
