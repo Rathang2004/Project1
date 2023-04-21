2e91eb78f423633b32d359dab9763ba8fbfcce37
“Rathang2004git config --global user.email “rikimpandit@gmail.com <rikimpandit@gmail.com>
2023-04-20 21:15:02 -0700

    All my html files are dynamic .hbs files All of them are linked to css pages, using {{css}} js i also linked similarly The url is now local3000 for all Partials are implemented headers,footers, and etc Nmp seems to also be working well!

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Rathang Pandit" });
+  res.render('index', { title: 'CSC 317 App', name:"Rathang Pandit", css: ['index1.css'], js: ['index.js']});
+});
+router.get("/login",function (req,res){
+  res.render('login', {title: "Login Page", css: ['login1.css']});
+});
+router.get("/registration",function (req,res){
+  res.render('registration', {title: "Registration Page", css: ['registration1.css'], js: ['registration.js']});
+});
+router.get("/postvideo",function (req,res){
+  res.render('postvideo', {title: "Post Video Page", css: ['postvideo1.css']});
 });
 
+router.get("/profile", function(req,res){
+  res.render('profile', {title: "Profile Page", css: ['profile1.css']});
+});
+
+router.get("/viewpost",function (req,res){
+  res.render('viewpost', {title: "View Post Page", css: ['viewpost1.css']});
+});
 module.exports = router;
